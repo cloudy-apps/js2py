fc875b37c0af95d7c9d1f1bf5f6ac4f493b9a883
Darlan Alves <me@darlanalv.es>
2024-04-26 15:15:21 +0200

    feat: add js2py dependency

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
-from js2py import eval_js
+from js2py import translate_js6
 
 class Server(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_body = self.rfile.read(content_length)
-
-        self.send_response(200)
-        self.send_header('Content-type', 'text/x-python; charset=utf-8')
-        self.end_headers()
-
         input = post_body.decode('utf-8')
-        response = eval_js(input)
-        self.wfile.write(response.encode('utf-8'))
+
+        try:
+            response = translate_js6(input)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/x-python; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(str(response).encode('utf-8'))
+        except Exception as e:
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write(str(e).encode('utf-8'))
 
 def run(port=0):
     server_address = ('', port)
